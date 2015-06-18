1cfa2bb92a57fe28f2a099498b1f1049f3183acd
Qvintvs <emperorview@gmail.com>
2015-06-18 15:33:29 +0800

    debug

--- Cluster/src/main/java/CIBGrp5/MapDAOImpl.java
+++ Cluster/src/main/java/CIBGrp5/MapDAOImpl.java
@@ -31,7 +31,7 @@ public class MapDAOImpl implements MapDAO {
 
     @Override
     public Boolean submitDAOTask(EntryEventType type, String name, Object key, Object value, Boolean replicate){
-	MapDAOTask t = new MapDAOTask(type, name, key, value, replicate, this.instance, this.mapListener);
+	MapDAOTask t = new MapDAOTask(type, name, key, value, replicate, this.mapListener);
 	Future<Boolean> result = this.ex.submitToKeyOwner(t, key);
 	try {
 	    return result.get();
@@ -75,67 +75,5 @@ public class MapDAOImpl implements MapDAO {
     @Override
     public Boolean delete(String name, Object key){
 	return this.submitDAOTask(EntryEventType.REMOVED, name, key, null, false);
-    }
-
-    /**
-     * 
-     *
-     */
-    public class MapDAOTask
-	implements Callable<Boolean>,
-		   // Serializable,
-		   HazelcastInstanceAware{
-	
-	private final EntryEventType type;
-	private final String name;
-	private final Object key;
-	private final Object value;
-	private final Boolean replicate;
-	private HazelcastInstance instance;
-	private final EntryListener mapListener;
-	
-	public MapDAOTask(EntryEventType type,
-			  String name,
-			  Object key,
-			  Object value,
-			  Boolean replicate,
-			  HazelcastInstance instance,
-			  EntryListener mapListener){
-	    this.type = type;
-	    this.name = name;
-	    this.key = key;
-	    this.value = value;
-	    this.replicate = replicate;
-	    this.setHazelcastInstance(instance);
-	    this.mapListener = mapListener;
-	}
-	
-	@Override
-	public void setHazelcastInstance(HazelcastInstance hazelcastInstance) {
-	    this.instance = hazelcastInstance;
-	}
-	
-	@Override
-	public Boolean call(){
-	    IMap map = this.instance.getMap(this.name);
-	    switch(this.type){
-	    case ADDED:
-		map.set(this.key, this.value);
-		map.addEntryListener(this.mapListener, true);
-	    case UPDATED:
-		map.put(this.key, this.value);
-		if(this.replicate){
-		    map.addEntryListener(this.mapListener, true);
-		}
-	    case REMOVED:
-		map.delete(this.key);
-	    }
-	    /**
-	     *  need improvement here
-	     *
-	     */
-	    return true;
-	}
-
     }
 }
